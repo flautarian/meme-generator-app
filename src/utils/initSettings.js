d229ca3602f5bb4cdbc96312d8943fc5a212a698
flautarian <fgiacconi.dev@gmail.com>
2025-05-15 17:32:37 +0200

    [FIX]-Added-better-init-settings-values

--- src/utils/initSettings.js
+++ src/utils/initSettings.js
@@ -1,4 +1,4 @@
 export default initSettings =
 {
-  valuesStored: '{ "staticBDrawer": false, "backgroundType": "lava", "dragableResizeMode": "1-square", "fontAutoResize": true, "fontType": "Impact", "limitWidth": false, "minWidth": 100, "maxWidth": 300, "limitHeight": false, "minHeight": 100, "maxHeight": 300}'
+  valuesStored: '{ "staticBDrawer": false, "backgroundType": "lava", "dragableResizeMode": "1-square", "fontAutoResize": true, "fontType": "Impact", "limitWidth": false, "minWidth": 100, "maxWidth": 700, "limitHeight": false, "minHeight": 100, "maxHeight": 450}'
 }
